81456c202ba8a2d02bf3802794d06cb36af69615
BrayanEAA <brayaneduardoarteaga@gmail.com>
2023-02-23 20:55:33 -0600

    programa
    estructura

--- tarea5/tarea05/Program.cs
+++ tarea5/tarea05/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("ingrese el valor inicial: ");
+int ValorInicial = int.Parse(Console.ReadLine());
+
+Console.WriteLine("ingrese el valor final: ");
+int ValorFinal = int.Parse(Console.ReadLine());
+
+Console.WriteLine("los numeros pares dentro del rango ingresado son: ");
+
+for (int i = ValorInicial; i <= ValorFinal; i++)
+{
+    if (i % 2 ==0)
+    {
+        Console.WriteLine(i + " ");
+    }
+}
+
